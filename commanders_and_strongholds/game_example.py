47921d36ce94b2cec341c91f605e1c8afe9468bc
mrtovsky <emerte.quant@gmail.com>
2019-01-21 02:57:57 +0100

    @add: Showing the full payoff matrix.

--- commanders_and_strongholds/game_example.py
+++ commanders_and_strongholds/game_example.py
@@ -3,7 +3,7 @@ from commanders_and_strongholds import CommanderGame
 
 def main():
     """Example game where the row player and column player has 4 and 3 units
-    respectively. The number of strongholds is equal 3."""
+    respectively. The number of strongholds is equal to 3."""
     g = CommanderGame([4, 3], 3)
     g.fit_army_orders()
     g.fit_game_matrix()
@@ -11,6 +11,8 @@ def main():
         [[4, 0, 0]],
         [[2, 1, 0], [1, 1, 1]],
     ])
+    print(f'Full game matrix: \n{g.game_matrix_}')
+
 
 if __name__ == '__main__':
     main()
